b8f00ee5f1e4339e909d5b48861c1661a143f3de
CCR5 <matt-gomes@live.com>
2023-10-21 15:55:55 -0300

    feat: adding mumbai network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,26 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.20"
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1_000
+      }
+    }
+  },
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [`${process.env.PRIVATE_KEY || ""}`],
+    }
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: `${process.env.POLYGONSCAN_APIKEY || ""}`
+    }
+  }
 };
 
 export default config;
